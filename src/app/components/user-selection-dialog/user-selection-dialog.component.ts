d6d31e4608da02ef01e6454b770cf3726bd6af11
Tomi Mavrin <tomi.mavrin@gmail.com>
2019-11-11 13:33:51 +0100

    select user popup

--- src/app/components/user-selection-dialog/user-selection-dialog.component.ts
+++ src/app/components/user-selection-dialog/user-selection-dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/services/user/auth.service';
 
 @Component({
   selector: 'app-user-selection-dialog',
@@ -6,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-selection-dialog.component.scss']
 })
 export class UserSelectionDialogComponent implements OnInit {
+  users: User[] = [
+    {
+      uid: 'user_uid1',
+      name: 'username1',
+      email: 'email1'
+    },
+    {
+      uid: 'user_uid2',
+      name: 'username2',
+      email: 'email2'
+    },
+    {
+      uid: 'user_uid3',
+      name: 'username3',
+      email: 'email3'
+    }
+  ];
+
   constructor() {}
 
   ngOnInit() {}
